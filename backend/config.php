3313604e6a35560c58ae372b2f702c6b10c9401b
Sz-Wei Chen <swchen1217@gmail.com>
2020-02-05 20:22:56 +0800

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -6,5 +6,5 @@ define("DB_USER","server");
 define("DB_PASS","uhbSDDhWcaRJEFUH");
 define("DB_DBNAME","wfcss");
 
-$db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_DBNAME.';charset=utf8', DB_USER, DB_PASS);
+$db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_DBNAME.';charset=utf8', DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_LOCAL_INFILE => true));
 ?>
